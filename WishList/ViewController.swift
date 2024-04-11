4c8143ca6fac657f74a2220eb6dd2746808d6c74
saengle <zohv817@gmail.com>
2024-04-11 14:01:34 +0900

    Start Project

---
+++ WishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by 쌩 on 4/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
